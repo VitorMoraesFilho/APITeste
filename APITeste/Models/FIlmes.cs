0a1f16a3dd3088ac99a025ceb76d69ffa2978316
Vitor de Moraes <vitordemoraesfilho@gmail.com>
2025-06-02 09:54:57 -0300

    filmes feitos

---
+++ APITeste/Models/FIlmes.cs
@@ -0,0 +1,12 @@
+﻿namespace APITeste.Models
+{
+    public class FIlmes
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Genero { get; set; }
+        public int AnoLancamento { get; set; }
+        public string Diretor { get; set; }
+        public double Nota { get; set; }
+    }
+}
